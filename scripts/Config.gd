28622d5353e6f6ae328739847baf947276473b02
476 <ritian41@gmail.com>
2026-08-11 00:08:48 +0800

    Add_Bole_Not_Complete

---
+++ scripts/Config.gd
@@ -0,0 +1,9 @@
+# Config.gd 全局配置文件，所有密钥都放这里，记得加入.gitignore！
+extends Node
+
+# 百度TTS配置
+const BAIDU_TTS_TOKEN = "24.325c99e04c42978e7f20efc351154136.2592000.1788844136.282335-124117000"
+const BAIDU_TTS_API = "https://tsn.baidu.com/text2audio"
+
+# 音色：103是度博文（成熟稳重男声），非常适合伯乐
+const TTS_PERSON = 6748
